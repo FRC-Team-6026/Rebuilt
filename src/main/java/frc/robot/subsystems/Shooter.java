8253548691f63e9579f37d97e623a68c1153d88f
MonteCrisply <pjmills785@gmail.com>
2026-02-07 16:45:32 -0600

    Shooter Consolidation
    Merged Feeder into Shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,9 +1,15 @@
 package frc.robot.subsystems;
 
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkBase.ControlType;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
 import frc.lib.Items.SparkMax.SparkController;
 import frc.lib.configs.Sparkmax.SparkControllerInfo;
 import frc.robot.Constants;
@@ -12,16 +18,42 @@ public class Shooter extends SubsystemBase {
     private SparkController shooterSpark;
     private RelativeEncoder shooterEncoder;
     private SparkClosedLoopController shooterController;
+    
+    private SparkController feederSpark;
+    private RelativeEncoder feederEncoder;
+    private SparkClosedLoopController feederController;
 
     public Shooter() {
         this.shooterSpark = new SparkController(Constants.Setup.shooterSpark, new SparkControllerInfo().shooter());
+        this.feederSpark = new SparkController(Constants.Setup.feederSpark, new SparkControllerInfo().feeder());
 
         this.shooterEncoder = shooterSpark.sparkEncode;
         this.shooterController = shooterSpark.sparkControl;
+        this.feederEncoder = feederSpark.sparkEncode;
+        this.feederController = feederSpark.sparkControl;
     }
 
-    // TODO - code start and stop functions
-    public void start() { }
+    public Command windup() { return new InstantCommand(() -> {
+        shooterController.setReference(1.0, ControlType.kVoltage);
+        feederController.setReference(0.0, ControlType.kVoltage);
+    }, this);}
 
-    public void stop() { }
+    public Command shoot(DoubleSupplier distance) { return Commands.run(() -> {
+        // TODO - check units, refigure if robot params change
+        double dist = distance.getAsDouble();
+
+        // double desiredSpeed = (dist-18.5)+((74-dist)/74)+31.4;
+        double desiredSpeed = dist*0.1;
+
+        shooterController.setReference(desiredSpeed, ControlType.kVelocity);
+        // Is 80% of value enough to start feeding stuff through?
+        if (shooterEncoder.getVelocity() > 0.8*desiredSpeed)
+            // This will enable, but not disable. Should be fine? Change if this is a problem
+            feederController.setReference(1.0, ControlType.kVoltage);
+    }, this);}
+
+    public Command stop() { return new InstantCommand(() -> {
+        shooterController.setReference(0.0, ControlType.kVoltage);
+        feederController.setReference(0.0, ControlType.kVoltage);
+    }, this);}
 }
